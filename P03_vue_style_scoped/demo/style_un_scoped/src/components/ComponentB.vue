357313ed09d89f5b0657e175d3ad42162239577d
Shawn Yuan <ysh83737@hotmail.com>
2022-03-18 16:54:30 +0800

    P03: 修改示例组件，添加公共样式

---
+++ P03_vue_style_scoped/demo/style_un_scoped/src/components/ComponentB.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="component-class">
+  <div class="component-title">这里是组件B</div>
+  <div class="component-content">
+    <p>Vite<span class="weak-text">（法语意为 "快速的"，发音 /vit/，发音同 "veet"）</span>是一种新型前端构建工具，能够显著提升前端开发体验。</p>
+    <p>它主要由两部分组成：</p>
+    <ul class="dotted-list">
+      <li class="dotted-list-item">一个开发服务器，它基于 原生 ES 模块 提供了 丰富的内建功能，如速度快到惊人的 模块热更新（HMR）。</li>
+      <li class="dotted-list-item">一套构建指令，它使用 Rollup 打包你的代码，并且它是预配置的，可输出用于生产环境的高度优化过的静态资源。</li>
+    </ul>
+    <p>Vite 意在提供开箱即用的配置，同时它的 插件 API 和 JavaScript API 带来了高度的可扩展性，并有完整的类型支持。</p>
+  </div>
+</div>
+</template>
+
+<style lang="scss">
+@import '../style/index.scss';
+</style>
